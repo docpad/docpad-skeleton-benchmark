003027b11be7b552d6f467b596dedcd995d4a213
Peter Flannery <pflannery@users.noreply.github.com>
2015-03-12 05:07:01 +0000

    replaces eco with jade
    relates to #1

--- generate-files.js
+++ generate-files.js
@@ -1,9 +1,9 @@
 // generate basic eco test documents in the ./src/documents folder
 var fs = require('fs');
-var template = fs.readFileSync('./template.html.eco');
+var template = fs.readFileSync('./template.html.jade');
 
 for(var index=0; index<1000; index++) {
-  fs.writeFile("./src/documents/test-"+index+".html.eco", template.toString().replace(/[#]/g, index), function(err) {
+  fs.writeFile("./src/documents/test-"+index+".html.jade", template.toString().replace(/[%]/g, index), function(err) {
     if (err) {
       return console.log(err);
     }
