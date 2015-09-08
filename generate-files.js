10547de9b50ebebe78c94d70bf0e6b241e47a428
Benjamin Lupton <b@lupton.cc>
2015-09-09 05:23:05 +0800

    make the generator a bit more dynamic

--- generate-files.js
+++ generate-files.js
@@ -1,11 +1,30 @@
-// generate basic eco test documents in the ./src/documents folder
-var fs = require('fs');
-var template = fs.readFileSync('./template.html.jade');
+/* eslint no-var:0, indent:0, no-console:0, no-sync:0 */
 
-for(var index=0; index<1000; index++) {
-  fs.writeFile("./src/documents/test-"+index+".html.jade", template.toString().replace(/[%]/g, index), function(err) {
-    if (err) {
-      return console.log(err);
-    }
-  });
+// move these to argv
+console.log(process.argv.length, process.argv)
+var total = 1000  // move to process.argv
+var template = 'jade'
+
+// read the file
+var fsUtil = require('fs')
+var templateSource = null
+switch ( template ) {
+    case 'jade':
+        templateSource = fsUtil.readFileSync('./template.html.jade')
+        break
+
+    default:
+        throw new Error('Unknown template: ' + template)
+}
+
+// prepare
+function logError (err) {
+	if ( err )  console.log(err.stack)
+}
+
+// write files
+for ( var index = 0; index < total; index++ ) {
+	var templateOutPath = './src/documents/test-' + index + '.html.jade'
+	var templateOutSource = templateSource.toString().replace(/[%]/g, index)
+	fsUtil.writeFile(templateOutPath, templateOutSource, logError)
 }
